45b8abdc1230adc7f155ebf6f6cf72c2c9729c17
hyeonjoo <z1108102059@gmail.com>
2025-09-22 14:01:58 +0900

    boardContainer

--- src/pages/Board/BoardPage.jsx
+++ src/pages/Board/BoardPage.jsx
@@ -1,5 +1,18 @@
+import Sidebar from "../../components/Sidebar";
+import Header from "../../components/Header";
+import styles from "../../styles/Board.module.css";
+
 function BoardPage() {
-  return <div>board page</div>;
+  return (
+    <div className="app">
+      <Sidebar />
+
+      <div className="main">
+        <Header />
+        <div className={styles.boardContainer}>a</div>
+      </div>
+    </div>
+  );
 }
 
 export default BoardPage;
