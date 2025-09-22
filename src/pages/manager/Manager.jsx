3a6174359d93f91ed64280d20554aaf9e9d44222
s5hyuun <wendy132@naver.com>
2025-09-22 15:27:44 +0900

    Header

--- src/pages/manager/Manager.jsx
+++ src/pages/manager/Manager.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Sidebar from "../../components/Sidebar";
 import Header from "../../components/Header";
+import styles from "../../styles/Manager.module.css";
 
 export default function Manager() {
   const [langOpen, setLangOpen] = useState(false);
@@ -14,7 +15,13 @@ export default function Manager() {
       <main className="main">
         <Header />
         <section className="content">
-          <div className="placeholder">관리자 페이지</div>
+          <div className="placeholder" style={{ color: "#1E3A8A" }}>관리자 페이지</div>
+          <div className={styles.btn}>
+            <button className="btn" style={{ color: "#2563EB" }} >관리자 대시보드</button>
+            <button className="btn" style={{ color: "#2563EB" }}>직원 관리</button>
+            <button className="btn" style={{ color: "#2563EB" }}>제안 관리</button>
+            <button className="btn" style={{ color: "#2563EB" }}>공지 관리</button>
+          </div>
         </section>
       </main>
     </div>
