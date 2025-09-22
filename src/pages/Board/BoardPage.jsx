d063e03421dd43cd032de17dfe553eb8621c673b
hyeonjoo <z1108102059@gmail.com>
2025-09-22 11:31:14 +0900

    text color

--- src/pages/Board/BoardPage.jsx
+++ src/pages/Board/BoardPage.jsx
@@ -1,17 +1,5 @@
-import Header from "../../components/Header";
-import SideBar from "../../components/Sidebar";
-import styles from "../../styles/Board.module.css";
-
 function BoardPage() {
-  return (
-    <div className={styles.boardContainer}>
-      <SideBar />
-      <div className={styles.rightSide}>
-        <Header />
-        <div className={styles.main}>main</div>
-      </div>
-    </div>
-  );
+  return <div>board page</div>;
 }
 
 export default BoardPage;
