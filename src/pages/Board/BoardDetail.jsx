04e379ca443d7713bb27fda5ea721ce363bde59a
hyeonjoo <z1108102059@gmail.com>
2025-09-23 13:52:59 +0900

    comment 해주세요

--- src/pages/Board/BoardDetail.jsx
+++ src/pages/Board/BoardDetail.jsx
@@ -3,42 +3,46 @@ import BoardComment from "./components/BoardComment";
 
 function BoardDetail({ suggestion, onClose }) {
   if (!suggestion) return null;
+  const {
+    title,
+    description,
+    created_at,
+    user_name,
+    department_name,
+    vote_count = 0,
+    comment_count = 0,
+    status,
+  } = suggestion;
   return (
     <div className={styles.overlay}>
       <div className={styles.modal}>
         <div className={styles.detailIPost}>
           <div className={styles.detailTop}>
             <div>
-              <div>제목</div>
+              <div>{title}</div>
               <div className={styles.detailTopIcons}>
                 <div>
                   <i class="fa-regular fa-user"></i>
-                  노래하는 물고기
+                  {user_name}
                 </div>
                 <div>
                   <i class="fa-regular fa-calendar"></i>
-                  2025-09-19
+                  {new Date(created_at).toLocaleDateString()}
                 </div>
                 <div>
                   <i class="fa-regular fa-building"></i>
-                  R&D
+                  {department_name}
                 </div>
               </div>
             </div>
             <div>
-              <div className={styles.status}>In progress</div>
+              <div className={styles.status}>{status}</div>
               <button onClick={onClose}>❌</button>
             </div>
           </div>
           <div className={styles.detailContent}>
             <div>제안 내용</div>
-            <div>
-              여기 내용 나옴 <br />
-              여기 내용 나옴 <br />
-              여기 내용 나옴
-              <br />
-              여기 내용 나옴
-            </div>
+            <div>{description}</div>
           </div>
         </div>
         <div className={styles.detailMiddle}>
