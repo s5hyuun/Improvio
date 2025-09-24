63e0370fbb90575670f4aef217a2266f8b437677
Your Name <you@example.com>
2025-09-24 11:48:06 +0900

    up

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BoardPage from "../src/pages/Board/BoardPage.jsx";
 import ManagerPage from "../src/pages/manager/Manager.jsx";
 import Header from "./components/Header.jsx";
-
-function LoginPage({ setIsLoggedIn }) {
-  return (
-    <div className="p-4">
-      <h2>로그인 페이지</h2>
-      <button className="btn" onClick={() => setIsLoggedIn(true)}>
-        로그인하기
-      </button>
-    </div>
-  );
-}
-
-function SignupPage() {
-  return (
-    <div className="p-4">
-      <h2>회원가입 페이지</h2>
-    </div>
-  );
-}
+import LoginPage from "./pages/login/login.jsx"
 
 export default function App() {
   // 👉 처음엔 로그인된 상태
@@ -29,16 +11,15 @@ export default function App() {
 
   return (
     <BrowserRouter>
-      <Header isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
 
       <Routes>
         <Route path="/board" element={<BoardPage />} />
         <Route path="/manager" element={<ManagerPage />} />
         <Route
-          path="/login"
+          path="/"
           element={<LoginPage setIsLoggedIn={setIsLoggedIn} />}
         />
-        <Route path="/signup" element={<SignupPage />} />
+        {/* <Route path="/signup" element={<SignupPage />} /> */}
       </Routes>
     </BrowserRouter>
   );
