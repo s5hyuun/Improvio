9ca492a3500f207908b3758934f4e0e9e634dbc5
hyeonjoo <z1108102059@gmail.com>
2025-09-24 16:02:04 +0900

    login

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BoardPage from "../src/pages/Board/BoardPage.jsx";
 import ManagerPage from "../src/pages/manager/Manager.jsx";
+import LoginPage from "./pages/Login/Login.jsx";
 
 export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/board" element={<BoardPage />} />
         <Route path="/manager" element={<ManagerPage />} />
+        <Route path="/" element={<LoginPage setIsLoggedIn={setIsLoggedIn} />} />
       </Routes>
     </BrowserRouter>
   );
