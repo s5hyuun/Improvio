526fdbc4d58db8673d43d159164f6a92820d57f4
mimmu524 <gsh244400@naver.com>
2025-10-01 15:14:35 +0900

    up

--- back/db.js
+++ back/db.js
@@ -3,8 +3,7 @@ import mysql from "mysql2/promise";
 export const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "1234",
-  port:3307,
+  password: "0000",
   database: "improvio_db",
   waitForConnections: true,
   connectionLimit: 5,
